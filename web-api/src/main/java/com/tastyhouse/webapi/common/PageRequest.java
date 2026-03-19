3d2b381c7e832bd95d0590d8576f748a2b1d7f37
kk920106 <kk920106@naver.com>
2026-03-20 07:23:50 +0900

    feat(common): PageRequest에 페이지 크기 유효성 검사를 추가하여 최대 100으로 제한

--- web-api/src/main/java/com/tastyhouse/webapi/common/PageRequest.java
+++ web-api/src/main/java/com/tastyhouse/webapi/common/PageRequest.java
@@ -1,5 +1,6 @@
 package com.tastyhouse.webapi.common;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
 public record PageRequest(
@@ -7,6 +8,10 @@ public record PageRequest(
         int page,
 
         @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다")
+        @Max(value = 100, message = "페이지 크기는 100 이하여야 합니다")
         int size
 ) {
+    public PageRequest {
+        if (size > 100) size = 100;
+    }
 }
